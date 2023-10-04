05196cdf919e8f9bfbe7239c5bfcc32b59c19c6c
azer7777 <azertyuio123a77@gmail.com>
2023-10-04 22:01:08 +0200

    update

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask,render_template,request,redirect,flash,url_for
 
+booked_places = {}
 
 def loadClubs():
     with open('clubs.json') as c:
@@ -43,17 +44,20 @@ def book(competition,club):
 
 @app.route('/purchasePlaces',methods=['POST'])
 def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
+    competition_name = request.form['competition']
+    competition = [c for c in competitions if c['name'] == competition_name][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
     current_places = int(competition['numberOfPlaces'])
-    if placesRequired > 12:
+    total_booked_places = booked_places.get(competition_name, 0)
+    if placesRequired > 12 or placesRequired + total_booked_places > 12:
         flash("You can't book more then 12 places")
     elif placesRequired > current_places:
         flash("Not enough places available.")
     else:
         competition['numberOfPlaces'] = current_places - placesRequired
         flash('Great-booking complete!')
+        booked_places[competition_name] = total_booked_places + placesRequired
     return render_template('welcome.html', club=club, competitions=competitions)
 
 
